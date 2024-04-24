bdfa843af988deeb4d03b7832d11d23c41c18e67
Devid Duma <devidd05@gmail.com>
2024-04-24 22:14:11 +0200

    checkout form with 4 sections and review order

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,11 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {CurrencyPipe} from "@angular/common";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CurrencyPipe
   ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
@@ -14,22 +17,72 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
-  }
+  constructor(private formBuilder: FormBuilder, private cartService: CartService) {}
 
   ngOnInit() {
+    this.cartPriceQuantity();
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [""],
         lastName: [""],
         email: [""]
-      })
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [""],
+        city: [""],
+        state: [""],
+        country: [""],
+        zipCode: [""]
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [""],
+        city: [""],
+        state: [""],
+        country: [""],
+        zipCode: [""]
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [""],
+        nameOnCard: [""],
+        cardNumber: [""],
+        securityCode: [""],
+        expirationMonth: [""],
+        expirationYear: [""],
+      }),
+
     });
+  }
+
+  private cartPriceQuantity() {
+    // subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+
+    // compute cart total price and quantity
+    this.cartService.computeCartTotals();
   }
 
   onSubmit() {
     console.log("Handling the submit button");
-    console.log(this.checkoutFormGroup.get("customer"));
+    console.log(this.checkoutFormGroup.get("customer")!.value);
+  }
+
+  copyShippingAddressToBillingAddress(event: any) {
+    if(event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+        .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
   }
 }
