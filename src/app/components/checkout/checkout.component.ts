a1e490a02e0ea2f4721b3410d15231f1822b5d03
Devid Duma <devidd05@gmail.com>
2024-05-02 17:51:13 +0200

    checkout form credit card validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -113,10 +113,26 @@ export class CheckoutComponent implements OnInit {
           ])
       }),
       creditCard: this.formBuilder.group({
-        cardType: [""],
-        nameOnCard: [""],
-        cardNumber: [""],
-        securityCode: [""],
+        cardType: new FormControl("",
+          [
+            Validators.required
+          ]),
+        nameOnCard: new FormControl("",
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Luv2ShopValidators.notOnlyWhitespace
+          ]),
+        cardNumber: new FormControl("",
+          [
+            Validators.required,
+            Validators.pattern("[0-9]{16}")
+          ]),
+        securityCode: new FormControl("",
+          [
+            Validators.required,
+            Validators.pattern("[0-9]{3}")
+          ]),
         expirationMonth: [""],
         expirationYear: [""],
       }),
@@ -221,6 +237,18 @@ export class CheckoutComponent implements OnInit {
   }
   get billingAddressCountry() {
     return this.checkoutFormGroup.get("billingAddress.country")!;
+  }
+  get creditCardType() {
+    return this.checkoutFormGroup.get("creditCard.cardType")!;
+  }
+  get creditCardNameOnCard() {
+    return this.checkoutFormGroup.get("creditCard.nameOnCard")!;
+  }
+  get creditCardNumber() {
+    return this.checkoutFormGroup.get("creditCard.cardNumber")!;
+  }
+  get creditCardSecurityCode() {
+    return this.checkoutFormGroup.get("creditCard.securityCode")!;
   }
 
   copyShippingAddressToBillingAddress(event: any) {
